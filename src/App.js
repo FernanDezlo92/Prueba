8a2eebb73543cbd4a1528dd7ebf2f9cd2a225ec0
FernanDezlo93 <fernandofag@correo.ugr.es>
2023-02-15 18:27:55 +0100

    part1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Mensaje from "./Mensaje";
 
 function App() {
+  const a = 3;
+  const b = 2;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Mensaje color="red" message="estamos trabajando en un curso" />
+      <h1> Titulo de la App</h1>
+      <strong>Estamos trabajando en ello</strong>
+      <div>
+        <p>El resultado es: </p>
+        {a + b}
+      </div>
     </div>
   );
 }
